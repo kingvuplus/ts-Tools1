4f95640d588578cd842544e12a79a13a05f3468a
kingvuplus <sabahomri1@outlook.fr>
2016-10-15 19:37:21 +0100

    Start Build

---
+++ usr/lib/enigma2/python/Plugins/TSimage/TSimagePanel/scripts/Delete_all_Crashlogs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#DESCRIPTION=This script will delete all crash-logs from /media/hdd
+echo "Removing enigma2 crashlogs..."
+rm -rf /media/hdd/enigma2_crash*
+echo "done."
+exit 0
---
+++ usr/script/DeleteAllCrashlogs.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rm -rf /media/hdd/enigma2_crash*
